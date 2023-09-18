e701e6d2cfcef894ace48a9c6e6b088032f630f4
VERTIGOJACK <vertigo.digital.design@gmail.com>
2023-09-18 18:39:50 +0200

    workshop example

--- App.js
+++ App.js
@@ -1,10 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
+  [userInput, setUserInput] = useState("");
+  [bookList, setBookList] = useState([]);
+
+  const handleInputChange = (value) => {
+    setUserInput(value);
+  };
+
+  const addBook = () => {
+    setBookList([...bookList, userInput]);
+    setUserInput("");
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.addContainer}>
+        {/* two way binding for userinput using value and onchangetext */}
+        <TextInput
+          onChangeText={handleInputChange}
+          value={userInput}
+          placeholder="Enter book name"
+          style={styles.textInput}></TextInput>
+        <Button title="Add" onPress={addBook}></Button>
+      </View>
+      <View style={styles.listView}>
+        {/* parentheses inside map function means result should be interpreted as JSX*/}
+        {bookList.map((item, index) => (
+          // every item should have a unique key
+          <Text key={index}>{item}</Text>
+        ))}
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -12,9 +40,20 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 50,
+    backgroundColor: "#fff",
+  },
+  addContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  textInput: {
+    width: "80%",
+    borderWidth: 1,
+    borderColor: "blue",
+  },
+  listView: {
+    flexDirection: "column",
+    justifyContent: "center",
   },
 });
